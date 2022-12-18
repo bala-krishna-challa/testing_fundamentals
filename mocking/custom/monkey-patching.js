32760be5fe6e853cb5d58b5b65ec5cb50bf0da1c
Bala Challa <balakrishnachalla@gmail.com>
2022-12-18 23:40:31 +0530

    adding deep assertion

--- mocking/custom/monkey-patching.js
+++ mocking/custom/monkey-patching.js
@@ -2,10 +2,24 @@ const assert = require("assert");
 const startGame = require("../game");
 const utils = require("../utils");
 
+const fn = (mockImplemention) => {
+  const mockFn = (...args) => {
+    mockFn.mock.calls.push(args);
+    return mockImplemention(...args);
+  };
+  mockFn.mock = { calls: [] };
+
+  return mockFn;
+};
+
 const originalUtilGetWinner = utils.getWinner;
-utils.getWinner = (p1, p2) => p1;
+utils.getWinner = fn((p1, p2) => p1);
 
 const winner = startGame("Player X", "Player Y");
 assert.strictEqual(winner, "Player X");
+assert.deepStrictEqual(utils.getWinner.mock.calls, [
+  ["Player X", "Player Y"],
+  ["Player X", "Player Y"],
+]);
 
 utils.getWinner = originalUtilGetWinner;
